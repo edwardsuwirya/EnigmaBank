2e30155f662c80e6e6be27c00f1a60557f45eff8
Edward Suwirya <edward_suwirya@yahoo.com>
2024-08-27 08:20:24 +0700

    Dependency Injection using ServiceCollection

---
+++ Infrastructure/Startup.cs
@@ -0,0 +1,27 @@
+using Application.Repositories;
+using Infrastructure.Contexts;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class Startup
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services
+            .AddDbContext<ApplicationDbContext>(opt =>
+                opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))
+            );
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        return services
+            .AddTransient(typeof(IReadRepositoryAsync<,>), typeof(ReadRepositoryAsync<,>))
+            .AddTransient(typeof(IWriteRepositoryAsync<,>), typeof(WriteRepositoryAsync<,>))
+            .AddTransient(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+    }
+}
